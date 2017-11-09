edb1083c4fda00fbda919535475183c07f166b51
CannedFish <lianggy0719@126.com>
2017-11-09 17:15:07 +0800

    fix bugs

--- vm_monitor_agent/agent_rabbit/setup.py
+++ vm_monitor_agent/agent_rabbit/setup.py
@@ -14,10 +14,10 @@ def main():
         if SYSTEM == 'Linux':
             if not os.path.exists(conf_dir):
                 os.makedirs(conf_dir)
-            os.system("cp ../../etc/agent-rabbit.conf %s" % conf_dir)
+            os.system("cp etc/agent-rabbit.conf %s" % conf_dir)
         elif SYSTEM == 'Windows':
             import win32file
-            win32file.CopyFile('../../etc/agent-rabbit.conf', conf_file_path, 0)
+            win32file.CopyFile('etc/agent-rabbit.conf', conf_file_path, 0)
         else:
             raise ValueError("We are not support %s now." % SYSTEM)
 
@@ -25,14 +25,10 @@ def main():
         os.makedirs(dir_to_be_monitored)
 
     db_conn = sqlite3.connect(db_file_path)
-    for sql_file in ['./scripts/info.sql', './scripts/messages.sql']:
+    for sql_file in ['vm_monitor_agent/agent_rabbit/scripts/info.sql', 'vm_monitor_agent/agent_rabbit/scripts/messages.sql']:
         with file(sql_file, 'r') as fd:
             sql_str = fd.read()
             cur = db_conn.cursor()
             cur.execute(sql_str)
     db_conn.commit()
     db_conn.close()
-
-if __name__ == '__main__':
-    main()
-
